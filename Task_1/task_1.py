422eadfbc097bb2e3c7b69c8fe7f025ddececd9c
jenil-amenity <jenil.z@amenitytech.ai>
2025-12-01 17:49:40 +0530

    Updated Fibo

--- Task_1/task_1.py
+++ Task_1/task_1.py
@@ -1,16 +1,17 @@
 #0 1 1 2 3 5 8 13 ...
+import time
 class Task_1 :
     def fibonacci(n):
         j, k = 0, 1
         i=0
-        a = []
         while i <= n :
-            a.append(j)
+            print(j)
             j, k = k, j + k
             i += 1
-        print(a)
 
 s = int(input("Enter a range: "))
+start = time.time()
 Task_1.fibonacci(s)
+print("\nExecution Time >> ",time.time() - start)
 
         
